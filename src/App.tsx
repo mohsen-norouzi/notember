8b9a1efa47de0e97fa106d69def7568a53cc43a6
Mohsen <mohsen.norouzi@live.com>
2022-03-29 15:32:06 +0430

    dep-add(deps): :heavy_plus_sign: Install and config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 const App = () => {
-  return <div>Notember</div>;
+  return (
+    <div>
+      <h1 className='text-emerald-600 text-2xl'>Notember</h1>
+    </div>
+  );
 };
 
 export default App;
