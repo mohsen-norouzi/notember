bd2b4c2bd36007d9dbbd353688a01bcd140b4f57
Mohsen <mohsen.norouzi@live.com>
2022-03-31 01:53:32 +0430

    feature: :sparkles: Provide graphql request client

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import './App.css';
 
 import { Layout } from 'components/ui';
 import { Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { NoteApp } from 'pages';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 1000
+    }
+  }
+});
 
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path='/' element={<NoteApp />} />
-      </Routes>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<NoteApp />} />
+        </Routes>
+      </Layout>
+    </QueryClientProvider>
   );
 };
 
