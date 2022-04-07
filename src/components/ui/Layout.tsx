b8977b0f965cf2f5b3a6d54cb9e9f05448ce9786
Mohsen <mohsen.norouzi@live.com>
2022-04-07 17:25:35 +0430

    ui: :lipstick: remove layout max-width for better ux in larger screens

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 export const Layout: FC = (props) => {
   return (
-    <Container maxWidth='xl' className='bg-gray-100 h-screen p-5'>
+    <Container maxWidth={false} className='bg-gray-100 h-screen p-5'>
       {props.children}
     </Container>
   );
