9cdf890ec970b8f945dc1869df00faf63718192f
Mohsen <mohsen.norouzi@live.com>
2022-04-23 14:02:28 +0430

    ui(style): :lipstick: Improve Login and Register style

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,12 @@
-import { Button, TextField, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  TextField,
+  Typography
+} from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 
 import logoImage from 'assets/images/logo.png';
@@ -53,63 +61,69 @@ export const RegisterPage = () => {
 
   return (
     <div className='h-full w-full flex flex-col flex-auto items-center justify-center'>
-      <img className='w-16 mb-5' src={logoImage} alt='logo' />
+      <Card sx={{ borderRadius: '1rem' }} className='shadow-xl animated fadeInUp'>
+        <CardContent>
+          <div className='flex flex-col items-center justify-center gap-5 w-full mb-5'>
+            <CardMedia component='img' src={logoImage} alt='logo' sx={{ width: 45 }}></CardMedia>
+            <Typography variant='h5'>Create an account</Typography>
+          </div>
 
-      <Typography variant='h5' className='mt-16 mb-24 font-semibold text-18 sm:text-24'>
-        Create an account
-      </Typography>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col justify-center items-center w-full gap-2'
-      >
-        <Controller
-          name='username'
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              placeholder='Username'
-              variant='outlined'
-              size='small'
-              sx={{ border: '1px solid !important' }}
-              autoFocus
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='bg-white flex flex-col justify-center items-center gap-2'
+          >
+            <Controller
+              name='username'
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  placeholder='Username'
+                  variant='outlined'
+                  size='small'
+                  sx={{ border: '1px solid !important' }}
+                  autoFocus
+                />
+              )}
             />
-          )}
-        />
 
-        <Controller
-          name='email'
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              placeholder='Email'
-              variant='outlined'
-              size='small'
-              sx={{ border: '1px solid !important' }}
-              autoFocus
+            <Controller
+              name='email'
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  placeholder='Email'
+                  variant='outlined'
+                  size='small'
+                  sx={{ border: '1px solid !important' }}
+                />
+              )}
             />
-          )}
-        />
 
-        <Controller
-          name='password'
-          control={control}
-          render={({ field }) => (
-            <TextField
-              {...field}
-              type='password'
-              placeholder='Password'
-              variant='outlined'
-              size='small'
-              sx={{ border: '1px solid !important' }}
+            <Controller
+              name='password'
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type='password'
+                  placeholder='Password'
+                  variant='outlined'
+                  size='small'
+                  sx={{ border: '1px solid !important' }}
+                />
+              )}
             />
-          )}
-        />
+          </form>
+        </CardContent>
 
-        <Button type='submit'>Login</Button>
-      </form>
+        <CardActions className='justify-center'>
+          <Button type='submit' size='medium'>
+            Register
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
