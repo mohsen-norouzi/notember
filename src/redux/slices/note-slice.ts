743c166f6ff3d06f221e67800b2defd45b24c40e
Mohsen <mohsen.norouzi@live.com>
2022-05-30 22:24:36 +0430

    feature: :sparkles: Refetch notes and labels every 15 seconds

--- src/redux/slices/note-slice.ts
+++ src/redux/slices/note-slice.ts
@@ -1,17 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { NoteEntity } from 'graphql/generated/graphql-types';
 
 interface noteState {
   showNoteForm: boolean;
+  notes: NoteEntity[];
 }
 
 const initialState: noteState = {
-  showNoteForm: false
+  showNoteForm: false,
+  notes: []
 };
 
 export const noteSlice = createSlice({
   name: 'note',
   initialState,
   reducers: {
+    setNotes: (state, action: PayloadAction<NoteEntity[]>) => {
+      state.notes = action.payload;
+    },
+
     showForm: (state) => {
       state.showNoteForm = true;
     },
